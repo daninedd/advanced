bcac7ad8df6a5c3a7d73348b6ec1b83e542f88ca
daninedd <546186941@qq.com>
2017-08-10 15:23:11 +0800

    add test

---
+++ frontend/models/EntryForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/9
+ * Time: 16:11
+ */
+
+namespace frontend\models;
+
+
+class EntryForm
+{
+
+}
---
+++ frontend/views/site/entry-confirm.php
@@ -0,0 +1,10 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \frontend\models\SignupForm */
+
+?>
+<body>
+h哈哈哈哈哈或或或或或或或或或或
+</body>
